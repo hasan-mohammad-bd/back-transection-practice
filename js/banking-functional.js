1437e0e5dc2e63506019ae130cf75ed2c12fa227
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-12 22:53:42 +0600

    function banking problem solve

--- js/banking-functional.js
+++ js/banking-functional.js
@@ -1,4 +1,68 @@
-function inputAmount(amount) {
+function inputAmount(inputID) {
     //to get input from inputBox
-    
+    const inputField = document.getElementById(inputID);
+    const inputFieldText = inputField.value;
+    const inputFiledValue = parseFloat(inputFieldText);
+
+
+    inputField.value = "";
+    return inputFiledValue;    
 }
+
+function amountAdding(amountID, amount){
+    //to get amount which will be added.
+    const amountAdding = document.getElementById(amountID);
+    const amountAddingText = amountAdding.innerText;
+    const previousAmountAdding = parseFloat(amountAddingText);
+    const theTotalAmount = previousAmountAdding + amount;
+    amountAdding.innerText = theTotalAmount
+    return amountAdding.innerText;
+}
+
+function balanceAmount(){
+    //get only the total balance value
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat(balanceTotalText);
+    return balanceTotalAmount;
+}
+
+function totalBalanceAmount(amount, addIf){
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalAmount = balanceAmount();
+    if(addIf == true){
+       const totalBalance = balanceTotalAmount + amount;
+       balanceTotal.innerText = totalBalance;
+       
+    }
+    else {
+        const totalBalance = balanceTotalAmount - amount;
+        balanceTotal.innerText = totalBalance;
+    }
+}
+
+
+document.getElementById("deposit-button").addEventListener("click",function(){
+    const inputValue = inputAmount("deposit-input");
+
+    if(inputValue > 0){
+        amountAdding("deposit-total", inputValue);
+        totalBalanceAmount(inputValue, true);
+    }
+   
+})
+
+document.getElementById("withdraw-button").addEventListener("click", function(){
+    const withdrawValue = inputAmount("withdraw-input");
+    const balanceTotal = balanceAmount();
+    if(withdrawValue > 0 && withdrawValue < balanceTotal){
+        amountAdding("withdraw-total", withdrawValue);  
+    } else {
+        alert("Please put valid amount or You don't have sufficient balance");
+
+    }
+
+
+
+})
+
